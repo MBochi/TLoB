d1e5df6378e0789a70e58b6fe82627ba003cb4d4
Yuliteg <Yuliteg@users.noreply.github.com>
2024-09-24 19:57:47 +0200

    changed way the player attacks

--- Assets/!/Scripts/PlayerCombat.cs
+++ Assets/!/Scripts/PlayerCombat.cs
@@ -19,11 +19,7 @@ public class PlayerCombat : MonoBehaviour
     public Transform attackPoint;
     public Transform attackPointAncor; // rotate object for aiming
     public LayerMask damageableLayers;
-    private bool showGizmo = false;
-    [Header("Mouse or XBox Controller")]
-    public Boolean switchAimControls = false;
     private Vector2 stickPos;
-    private Vector2 mousePos;
     private Rigidbody2D rb;
     private Stats playerStats;
     public static float chargeMax = 1f;
@@ -39,85 +35,72 @@ public class PlayerCombat : MonoBehaviour
         Aim();
         Timer();
         UpdateChargeBar();
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        stickPos = new Vector2(Input.GetAxisRaw("RightJoyStickHorizontal"), Input.GetAxisRaw("RightJoyStickVertical"));
+        stickPos = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
 
     private void Aim()
     {
-        if (switchAimControls == false) // Keyboard
+        if (Input.GetKeyDown(KeyCode.JoystickButton3) && !chargedAttackTimerActive)
         {
-            Vector2 lookDir = mousePos - rb.position;
-            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
-            attackPointAncor.rotation = Quaternion.Euler(0, 0, angle);
-            showGizmo = true;
-
+            attackMode += 1;
+            if (attackMode > 1)
+            {
+                attackMode = 0;
+            }
         }
-        else if (switchAimControls == true) //Controller
+    
+        float stickAngle = Mathf.Atan2(stickPos.y, stickPos.x) * Mathf.Rad2Deg;
+        attackPointAncor.rotation = Quaternion.Euler(0, 0, stickAngle);
+        
+        if(Input.GetKeyDown(KeyCode.JoystickButton2))
         {
-            if (Input.GetKeyDown(KeyCode.JoystickButton3) && !chargedAttackTimerActive)
+            chargedAttackTimerActive = true;
+            chargeBar.SetActive(true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.JoystickButton2))
+        {   
+            if (chargedAttackTimerActive)
             {
-                attackMode += 1;
-                if (attackMode > 1)
+                chargeBar.SetActive(false);
+                if (attackMode == 0) // Melee
                 {
-                    attackMode = 0;
-                }
-            }
-            Vector2 idleStickPos = new(Mathf.Round(stickPos.x), Mathf.Round(stickPos.y));
-            float stickAngle = Mathf.Atan2(stickPos.y, stickPos.x) * Mathf.Rad2Deg;
-            if (idleStickPos != Vector2.zero) // Player aimes with stick
-            {
-                showGizmo = true;
-                attackPointAncor.rotation = Quaternion.Euler(0, 0, stickAngle * -1);
-                chargedAttackTimerActive = true;
-                chargeBar.SetActive(true);
-            }
-            else
-            {   
-                // if player released stick
-                if (chargedAttackTimerActive)
-                {
-                    chargeBar.SetActive(false);
-                    if (attackMode == 0) // Melee
+                    GameObject slash = Instantiate(SlashPrefab, attackPointAncor);
+                    slash.transform.SetParent(this.transform);
+                    if (chargedAttackTimer > chargeMax)
                     {
-                        GameObject slash = Instantiate(SlashPrefab, attackPointAncor);
-                        slash.transform.SetParent(this.transform);
-                        if (chargedAttackTimer > chargeMax)
+                        slash.GetComponent<SpriteRenderer>().color = new Color(1,0,0,1);
+                    }
+                    // Check if enemy is hit and apply damage
+                    Collider2D[] hitObjecsts = Physics2D.OverlapCircleAll(attackPoint.position, attackRadius, damageableLayers);
+                    foreach (Collider2D collision_object in hitObjecsts)
+                    {
+                        if(collision_object.gameObject.tag == "Enemy")
                         {
-                            slash.GetComponent<SpriteRenderer>().color = new Color(1,0,0,1);
-                        }
-                        // Check if enemy is hit and apply damage
-                        Collider2D[] hitObjecsts = Physics2D.OverlapCircleAll(attackPoint.position, attackRadius, damageableLayers);
-                        foreach (Collider2D collision_object in hitObjecsts)
-                        {
-                            if(collision_object.gameObject.tag == "Enemy")
+                            if (chargedAttackTimer > chargeMax)
+                            {       
+                                collision_object.GetComponent<EnemyCombat>().TakeDamage(playerStats.GetAttackDamage() * 2);
+                            }
+                            else
                             {
-                                if (chargedAttackTimer > chargeMax)
-                                {       
-                                    collision_object.GetComponent<EnemyCombat>().TakeDamage(playerStats.GetAttackDamage() * 2);
-                                }
-                                else
-                                {
-                                    collision_object.GetComponent<EnemyCombat>().TakeDamage(playerStats.GetAttackDamage());
-                                }
+                                collision_object.GetComponent<EnemyCombat>().TakeDamage(playerStats.GetAttackDamage());
                             }
                         }
                     }
-                    else if (attackMode == 1) // ranged
-                    {
-                        GameObject arrow = Instantiate(ArrowPrefab, attackPoint);
-                        if (chargedAttackTimer > chargeMax)
-                        {
-                            chargedAttackTimer = chargeMax;
-                        } 
-                        arrow.GetComponent<Arrow>().SetProjectileDamage(playerStats.GetAttackDamage());
-                        arrow.GetComponent<Rigidbody2D>().velocity = new Vector2(attackPoint.position.x - attackPointAncor.position.x, attackPoint.position.y - attackPointAncor.position.y) * chargedAttackTimer * 10;
-                        arrow.transform.parent = null;
-                    }
                 }
-                chargedAttackTimerActive = false;
-                showGizmo = false;
+                else if (attackMode == 1) // ranged
+                {
+                    GameObject arrow = Instantiate(ArrowPrefab, attackPoint);
+                    if (chargedAttackTimer > chargeMax)
+                    {
+                        chargedAttackTimer = chargeMax;
+                    } 
+                    arrow.GetComponent<Arrow>().SetProjectileDamage(playerStats.GetAttackDamage());
+                    arrow.GetComponent<Rigidbody2D>().velocity = new Vector2(attackPoint.position.x - attackPointAncor.position.x, attackPoint.position.y - attackPointAncor.position.y) * chargedAttackTimer * 10;
+                    arrow.transform.parent = null;
+                }
             }
+            chargedAttackTimerActive = false;
         }
     }
 
@@ -143,11 +126,7 @@ public class PlayerCombat : MonoBehaviour
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        if (showGizmo)
-        {
-            Gizmos.DrawWireSphere(attackPoint.position, attackRadius);
-        }
-        
+        Gizmos.DrawWireSphere(attackPoint.position, attackRadius);
     }
 
     public int GetAttackMode()
