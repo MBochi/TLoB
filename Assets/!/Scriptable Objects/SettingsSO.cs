6b970327cf35549e61551538ec3174b3ef918ad6
MBochi <107650864+MBochi@users.noreply.github.com>
2024-10-19 11:35:33 +0200

    Fixed VolumeSlider, added Settings ScriptableObject

---
+++ Assets/!/Scriptable Objects/SettingsSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Settings", menuName = "Settings")]
+public class SettingsSO : ScriptableObject
+{
+        public float volume;
+}
